be4c4aaa99e2ca4fc38f0f9ac3b80d5a9da19380
pakoito <jueces.jus@gmail.com>
2016-05-02 19:53:26 +0100

    Fix proper copyright formatting

--- tests/src/androidTest/java/com/pacoworks/rxpaper/RxPaperBookTest.java
+++ tests/src/androidTest/java/com/pacoworks/rxpaper/RxPaperBookTest.java
@@ -1,12 +1,28 @@
 
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2016 pakoito & 2015 César Ferreira
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit  persons to whom the Software
+ * is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package com.pacoworks.rxpaper;
 
-import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
-
-import com.pacoworks.rxpaper.sample.MainActivity;
-import com.pacoworks.rxpaper.sample.model.ComplexObject;
-import com.pacoworks.rxpaper.sample.model.ImmutableObject;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -14,12 +30,17 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.List;
-
 import rx.Completable;
 import rx.Subscriber;
 import rx.observers.TestSubscriber;
 import rx.schedulers.Schedulers;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.pacoworks.rxpaper.sample.MainActivity;
+import com.pacoworks.rxpaper.sample.model.ComplexObject;
+import com.pacoworks.rxpaper.sample.model.ImmutableObject;
 
 @RunWith(AndroidJUnit4.class)
 public class RxPaperBookTest {
